c221b704735a970f644379ff727f67b414a59a3f
yonghee Kim <hjhhijk@gmail.com>
2023-06-30 15:38:55 +0900

    Update f1.py

--- temp/f1.py
+++ temp/f1.py
@@ -1,10 +1,3 @@
 print("f1")
 print("develop2")
 print("Augmentation3")
-<<<<<<< HEAD
-## release v1
-print("will be crash")
-=======
-# release v1
-print("crash in master")
->>>>>>> 6d5f9f1a48adee15609c02ae24ec395707537d42
